1be5e18683c3631b1c2d34de8f7d904afedd6b58
tborg <trevor.borg@gmail.com>
2013-11-11 21:24:33 -0800

    changing config setup to use env vars

--- server/config.py
+++ server/config.py
@@ -1,1 +1,19 @@
-config_tborglocal.py
+from os import environ
+
+
+class Config(dict):
+    SQLALCHEMY_POOL_RECYCLE = 3600
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    CONNECTIONS = {'richtimes': {'host': environ['RICHTIMES_MYSQL_HOST'],
+                                 'port': environ['RICHTIMES_MYSQL_PORT'],
+                                 'user': environ['RICHTIMES_MYSQL_USER'],
+                                 'password': environ['RICHTIMES_MYSQL_PASS'],
+                                 'database': 'richtimes'}}
+
+    SQLALCHEMY_BINDS = {key: "mysql+mysqldb://%(user)s:%(password)s@%(host)s/%(database)s?charset=utf8" % value for key, value in CONNECTIONS.iteritems()}
+
+    APP_ROOT_FROM_MAIN = '../../'
+
+    XML_DIR = APP_ROOT_FROM_MAIN + '/bower_components/richtimes-xml'
+    JSON_DIR = APP_ROOT_FROM_MAIN + '/app/json'
